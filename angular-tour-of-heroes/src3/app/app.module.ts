0e3f9bc8854a9e72033db3b69bd2f4dd64cf21f7
meviwm <meme098>
2018-01-12 17:38:57 +0800

    create service

--- angular-tour-of-heroes/src3/app/app.module.ts
+++ angular-tour-of-heroes/src3/app/app.module.ts
@@ -5,7 +5,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './service/hero.service';
+import { MessageService } from './service/message/message.service';
 
 
 @NgModule({
@@ -18,7 +20,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
